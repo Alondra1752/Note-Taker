cea4c9f5c62a4782b305defa474ac05ee8c5cf83
Alondra1752 <alondrasant7739@gmail.com>
2024-08-25 15:45:27 -0500

    changed paths for folders

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
 const notes = require("express").Router();
-const {readFromFile, readAndAppend} = require("../helpers/fsItils")
+const {readFromFile, readAndAppend} = require("../helpers/fsUtils")
 const uuid = require("../helpers/uuid");
 
 notes.get("/", (req, res) => {
